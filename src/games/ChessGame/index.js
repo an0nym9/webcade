d385c2ce8444f465be28db6a74724f4a10154c9d
an0nym9 <aeron.asug@icloud.com>
2026-03-07 23:37:01 -0700

    chore(src): add chess pieces and set positions for boxes

--- src/games/ChessGame/index.js
+++ src/games/ChessGame/index.js
@@ -1,6 +1,6 @@
 window.startChessGame = function () {
+    loadPieces(loadBoard());
     console.log("Chess Game loaded.");
-    loadBoard();
 }
 
 function loadBoard() {
@@ -10,20 +10,66 @@ function loadBoard() {
         height: "50px",
     };
     board.className = "board-container";
+    const positions = [];
     let shade = true;
     for (let j = 8; j >= 1; j--) {
         for (let i = 'A'.charCodeAt(0); i <= 'H'.charCodeAt(0); i++) {
             const box = document.createElement("div");
             const pos = `${String.fromCharCode(i)}${j}`;
+            box.id = pos;
             box.name = pos;
             box.textContent = pos;
-            box.className = shade ? 'black' : '';
+            box.className = "box" + (shade ? '-shaded' : '');
             board.appendChild(box);
             shade = !shade;
+            positions.push(pos);
         }
         shade = !shade;
     }
     board.style.gridTemplateColumns = (new Array(8).fill(boxSize.width)).join(' ');
     board.style.gridTemplateRows = (new Array(8).fill(boxSize.height)).join(' ');
     document.body.appendChild(board);
+    return positions;
+}
+
+function loadPieces(positions) {
+    const pieces = {
+        pawn: { symbol: "P", points: 1, count: 8, },
+        rook: { symbol: "R", points: 2, count: 2, },
+        knight: { symbol: "N", points: 2, count: 2, },
+        bishop: { symbol: "B", points: 2, count: 2, },
+        queen: { symbol: "Q", points: 5, count: 1, },
+        king: { symbol: "K", points: 30, count: 1, },
+    };
+    for (const position of positions) {
+        const [col, row] = position.split('');
+        if (!([8, 7, 2, 1].includes(+row))) continue;
+        const box = document.getElementById(position);
+        const piece = document.createElement("div");
+        piece.id = ["piece", +row < 4 ? "white" : "black", position].join('-');
+        piece.className = "piece" + (+row < 4 ? "-black" : '');
+        if ([8, 1].includes(+row)) {
+            switch (col) {
+                case 'A': case 'H':
+                    piece.textContent = pieces["rook"]["symbol"];
+                    break;
+                case 'B': case 'G':
+                    piece.textContent = pieces["knight"]["symbol"];
+                    break
+                case 'C': case 'F':
+                    piece.textContent = pieces["bishop"]["symbol"];
+                    break;
+                default:
+                    piece.textContent =
+                        +row === 8 ?
+                            col === 'D'
+                                ? pieces["queen"]["symbol"]
+                                : pieces["king"]["symbol"]
+                            : col === 'D'
+                                ? pieces["king"]["symbol"]
+                                : pieces["queen"]["symbol"];
+            }
+        } else piece.textContent = pieces["pawn"]["symbol"];
+        box?.appendChild(piece);
+    }
 }
